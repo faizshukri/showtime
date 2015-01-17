279f6c486ad35d79def1db9635dbeb48202ff79e
Faiz Shukri <faizshukri90@gmail.com>
2015-01-17 16:03:14 +0000

    Remove unwanted controller and add new one

---
+++ Showtime/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Showtime
+//
+//  Created by Faiz Shukri on 1/8/15.
+//  Copyright (c) 2015 Faiz Shukri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
