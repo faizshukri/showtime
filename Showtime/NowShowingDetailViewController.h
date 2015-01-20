d3ec30bfc10d486daaa0c64fe6f50e03af26d1e8
QAini <chocetti@gmail.com>
2015-01-20 02:23:07 +0000

    Initial tab bar

--- Showtime/NowShowingDetailViewController.h
+++ Showtime/NowShowingDetailViewController.h
@@ -17,4 +17,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *genresLabel;
 @property (weak, nonatomic) IBOutlet UILabel *pgAndRating;
 
+@property (weak, nonatomic) IBOutlet UITabBarItem *tabBar1;
+
+
 @end
