597555eccea264a8efb57c43580dd9e0c2a9183d
QAini <chocetti@gmail.com>
2015-01-21 01:55:08 +0000

    Tab Bar is almost functioning. (Kena tambah view utk individual tab item)

---
+++ ReviewsViewController.h
@@ -0,0 +1,16 @@
+//
+//  ReviewsViewController.h
+//  Showtime
+//
+//  Created by QAini on 1/20/15.
+//  Copyright (c) 2015 Faiz Shukri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReviewsViewController : UIViewController<UITabBarDelegate>
+
+
+
+
+@end
