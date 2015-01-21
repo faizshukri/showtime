cd592580b0b5a032cf29971f1af72128d3475308
Faiz Shukri <afahmadshukri1@sheffield.ac.uk>
2015-01-21 01:31:48 +0000

    Add upcoming collection view
    Signed-off-by: Faiz Shukri <afahmadshukri1@sheffield.ac.uk>

---
+++ Showtime/UpcomingCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  UpcomingCollectionViewController.h
+//  Showtime
+//
+//  Created by Faiz Shukri on 1/21/15.
+//  Copyright (c) 2015 Faiz Shukri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UpcomingCollectionViewController : UICollectionViewController
+
+@end
