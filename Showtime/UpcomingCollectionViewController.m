ca406f660a9bcdfca992c4bfc33c28e6c62aeb77
Faiz Shukri <afahmadshukri1@sheffield.ac.uk>
2015-01-22 00:28:11 +0000

    Connect upcoming detail to now showing detail
    Signed-off-by: Faiz Shukri <afahmadshukri1@sheffield.ac.uk>

--- Showtime/UpcomingCollectionViewController.m
+++ Showtime/UpcomingCollectionViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "UpcomingCollectionViewController.h"
+#import "NowShowingDetailViewController.h"
 #import "Movie.h"
 
 @interface UpcomingCollectionViewController ()
@@ -39,15 +40,20 @@ static NSString * const reuseIdentifier = @"Cell";
     // Dispose of any resources that can be recreated.
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    UICollectionViewCell *cell = (UICollectionViewCell*) sender;
+    NSIndexPath *indexPath = [self.collectionView indexPathForCell:cell];
+    
+    NowShowingDetailViewController *showDetail = [segue destinationViewController];
+    [showDetail setMovie:[[[_movieInSection allValues] objectAtIndex:indexPath.section] objectAtIndex:indexPath.row]];
 }
-*/
+
 
 #pragma mark <UICollectionViewDataSource>
 
